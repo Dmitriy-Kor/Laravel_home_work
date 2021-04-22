35f2c5af400012eaa4507ec04a8efffb95407b56
Dmitriy Koreshkov <kore.dmi@gmail.com>
2021-04-22 17:35:52 +0300

    HW8

--- example-app/app/Providers/RouteServiceProvider.php
+++ example-app/app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -18,6 +19,9 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+
+    public const ACCOUNT = '/account';
+
 
     /**
      * The controller namespace for the application.
